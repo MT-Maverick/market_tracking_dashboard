8eba1b655b7134e3327ac96aefaa1c509524e85f
MT-Maverick <mfundosindane0@gmail.com>
2025-01-16 21:26:16 +0000

    Included side panel to display share price

--- market_tracking_dashboad.py
+++ market_tracking_dashboad.py
@@ -1,5 +1,4 @@
-import dash
-from dash import dcc, html, Output, Input 
+from dash import dash, dcc, html, Output, Input
 import yfinance as yf
 import plotly.graph_objects as go
 import pandas as pd
@@ -9,33 +8,62 @@ app = dash.Dash(__name__)
 
 app.layout = html.Div(
     children=[
-        html.H1(children="JSE Stock Candlestick Chart"),
-        dcc.Graph(id="candlestick-chart"),
-        dcc.Interval(id="interval-component", interval=60 * 1000, n_intervals=0),  # Update every minute
+        html.Div(
+            children=[
+                html.H2("Current Share Value", style={"textAlign": "center"}),
+                html.Div(id="current-share-value", style={"fontSize": "24px", "textAlign": "center"}),
+            ],
+            style={
+                "width": "10%",
+                "float":"Left",
+                "height": "100vh",
+                "backgroundColor": "#f0f0f0", 
+                "padding": "20px",
+            },
+        ),
+        html.Div(
+            children=[
+                html.H1("JSE Stock Candlestick Chart", style={"textAlign": "center"}),
+                dcc.Graph(id="candlestick-chart"),
+                dcc.Interval(id="interval-component", interval=60 * 1000, n_intervals=0),
+            ],
+            style={
+                "width": "80%",
+                "float": "right",
+                "height": "100vh",
+                "padding": "10px",
+            },
+        ),
     ],
-    style={"width": "80%", "margin": "auto","overflowX":"hidden"}
+    style={"overflow": "hidden"},  
 )
 
-@app.callback(Output("candlestick-chart", "figure"), Input("interval-component", "n_intervals"))
-def update_candlestick_chart(n):
-    jse = yf.Ticker('JSE.JO')
-    df = pd.DataFrame(jse.history(period='1mo'))  # Fetch data for the past month
+@app.callback(
+    Output("current-share-value", "children"),
+    Output("candlestick-chart", "figure"),
+    Input("interval-component", "n_intervals")
+)
+def update_data(n):
+    jse = yf.Ticker("JSE.JO")
+    current_price = jse.history_metadata.get("regularMarketPrice")  
+
+    df = pd.DataFrame(jse.history(period="1mo"))
+    print("current_price,df")
     fig = go.Figure(data=[go.Candlestick(x=df.index,
                 open=df['Open'],
                 high=df['High'],
                 low=df['Low'],
                 close=df['Close'])])
     fig.update_layout(
-    xaxis=dict(
-        rangeslider=dict(visible=False),  # Hide the rangeslider
-        type="category",  # Treat x-axis as categorical for better scrolling
-        tickmode='array',
-        tickvals=df.index,
-        ticktext=df.index.strftime('%d')  # Format to display only the day
-    ),
- 
+        xaxis=dict(
+            rangeslider=dict(visible=False),
+            type="category",
+            tickmode="array",
+            tickvals=df.index,
+            ticktext=df.index.strftime("%d"),
+        ),
     )
-    return fig
+    return  f"R {current_price}",fig  
 
 if __name__ == "__main__":
-    app.run_server(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8888)))
+    app.run_server(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8888)))
